7f2c62dc7aa33263d263da4bd789d2d0a8308177
Colin Bethea <colinjbethea@gmail.com>
2021-10-11 12:30:12 -0400

    Update format for problems

--- problemset/217.py
+++ problemset/217.py
@@ -1,7 +1,16 @@
-'''
-https://leetcode.com/problems/contains-duplicate/
-'''
+# https://leetcode.com/problems/contains-duplicate/
 
 class Solution(object):
-    def containsDuplicate(self, nums):
-      return len(set(nums)) != len(nums)
+
+    '''
+    Solution #1 - Set
+    ---
+    Use a set to retrieve unique elements in input array. Compare the length of the set to the length of input array.
+    If they are the same, there is no duplicate. If they differ, there is a duplicate.
+    ---
+    R: O(n), where n is # of elements in (nums)
+    S: O(n), where n is # of elements in (nums)
+    '''
+
+    def containsDuplicateWithSet(self, nums):
+        return len(set(nums)) != len(nums)
